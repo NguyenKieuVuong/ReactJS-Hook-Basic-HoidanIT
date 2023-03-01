31a8bbbf44ced6c36b610485c950d06ae72e5548
Vothuong <nguyenkieuvuong@gmail.com>
2023-03-01 14:35:59 +0700

    State & useState trong Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 function App() {
+  let [name, setName] = useState("Vương");
+  let [address, setAddress] = useState();
+  const handleOnClick = () => {
+    setName(address);
+    console.log("alert");
+  };
+  const handleOnChange = (event) => {
+    setAddress(event.target.value);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
+        <h1>Hello world with React and {name}</h1>
+        <div className="">
+          <input
+            type="text"
+            value={address}
+            onChange={(event) => handleOnChange(event)}
+          />
+        </div>
+        <button type="button" onClick={(event) => handleOnClick(event)}>
+          summit
+        </button>
       </header>
     </div>
   );
